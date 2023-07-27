9e691f8fa88fae95f11a33e64149b9b35f52a697
fredlapolla <fred.lapolla@med.nyu.edu>
2023-07-27 10:30:58 -0400

    Update VilcekMasters_6_BioinformaticsVisualization_GroupWork.R

--- VilcekMasters_6_BioinformaticsVisualization_GroupWork.R
+++ VilcekMasters_6_BioinformaticsVisualization_GroupWork.R
@@ -2,7 +2,7 @@
 
 #1. Convert the following data into a matrix, with rownames for the genes being named. Re-order the columns to group WT and KO mice strains together. Make subsets of highly expressed genes (choose your own threshold) 
 
-dolgalevNormRNA <- read.csv("https://raw.githubusercontent.com/fredlapolla/RVilcekMasters2021/main/counts.normalized.csv")
+dolgalevNormRNA <- read.csv("https://raw.githubusercontent.com/fredlapolla/RVilcekMaster23/main/counts.normalized.csv")
 
 
 #2. Try making a heatmap of the data you subsetted in part 1. Try both clustering by column and setting this setting to false. Explore the help information and see what other row clustering methods are available
